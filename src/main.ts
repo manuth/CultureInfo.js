42b9df97f6644756deecb8f5633dfbe15dde354e
Manuel Thalmann <m@nuth.ch>
2018-06-12 14:52:17 +0200

    Remove useless comment

--- src/main.ts
+++ src/main.ts
@@ -1,13 +1,2 @@
 import CultureInfo from "./CultureInfo";
-
-/**
- * This is your module: culture-info
- * 
- * Export the components you want to provide using the `export` keyword.
- * Other people can use it then using
- * 
- * ```ts
- * import * as cultureinfo from 'culture-info';
- * cultureinfo.Example();
- */
 export default CultureInfo;
