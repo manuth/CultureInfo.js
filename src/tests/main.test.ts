e23aef0856564d2159f71370cb3b3afa3e27a6d9
Manuel Thalmann <m@nuth.ch>
2019-01-05 17:58:53 +0100

    Normalize the naming of variables

--- src/tests/main.test.ts
+++ src/tests/main.test.ts
@@ -1,4 +1,4 @@
-import assert = require("assert");
+import Assert = require("assert");
 import { CultureInfo } from "..";
 
 suite("CultureInfo", () =>
@@ -7,47 +7,47 @@ suite("CultureInfo", () =>
         "Checking whether the case of the case of cultures are converted correctly...",
         () =>
         {
-            assert.strictEqual(new CultureInfo("En-uS").Name, "en-US");
-            assert.strictEqual(new CultureInfo("DE-Ch").Name, "de-CH");
-            assert.strictEqual(new CultureInfo("zH-hAnS-cn").Name, "zh-Hans-CN");
+            Assert.strictEqual(new CultureInfo("En-uS").Name, "en-US");
+            Assert.strictEqual(new CultureInfo("DE-Ch").Name, "de-CH");
+            Assert.strictEqual(new CultureInfo("zH-hAnS-cn").Name, "zh-Hans-CN");
         });
 
     test(
         "Checking whether parents are determined correctly...",
         () =>
         {
-            assert.strictEqual(new CultureInfo("de-CH").Parent.Name, "de");
-            assert.strictEqual(new CultureInfo("en-GB").Parent.Name, "en");
-            assert.strictEqual(new CultureInfo("zh-Hans-CN").Parent.Name, "zh-Hans");
-            assert.strictEqual(new CultureInfo("zh-Hans-CN").Parent.Parent.Name, "zh");
+            Assert.strictEqual(new CultureInfo("de-CH").Parent.Name, "de");
+            Assert.strictEqual(new CultureInfo("en-GB").Parent.Name, "en");
+            Assert.strictEqual(new CultureInfo("zh-Hans-CN").Parent.Name, "zh-Hans");
+            Assert.strictEqual(new CultureInfo("zh-Hans-CN").Parent.Parent.Name, "zh");
         });
 
     test(
         "Checking \"CultureInfo to string\"-conversion...",
         () =>
         {
-            assert.strictEqual(new CultureInfo("en-US").toString(), "en-US");
-            assert.strictEqual(new CultureInfo("de-CH").toString(), "de-CH");
-            assert.strictEqual(new CultureInfo("zh-Hans-CN").toString(), "zh-Hans-CN");
+            Assert.strictEqual(new CultureInfo("en-US").toString(), "en-US");
+            Assert.strictEqual(new CultureInfo("de-CH").toString(), "de-CH");
+            Assert.strictEqual(new CultureInfo("zh-Hans-CN").toString(), "zh-Hans-CN");
         });
 
     test(
         "Checking the integrity of the IsNeutralCulture-property...",
         () =>
         {
-            assert.strictEqual(new CultureInfo("de-CH").IsNeutralCulture, false);
-            assert.strictEqual(new CultureInfo("de-CH").Parent.IsNeutralCulture, true);
-            assert.strictEqual(new CultureInfo("zh-Hans-CN").IsNeutralCulture, false);
-            assert.strictEqual(new CultureInfo("zh-Hans-CN").Parent.Parent.IsNeutralCulture, true);
+            Assert.strictEqual(new CultureInfo("de-CH").IsNeutralCulture, false);
+            Assert.strictEqual(new CultureInfo("de-CH").Parent.IsNeutralCulture, true);
+            Assert.strictEqual(new CultureInfo("zh-Hans-CN").IsNeutralCulture, false);
+            Assert.strictEqual(new CultureInfo("zh-Hans-CN").Parent.Parent.IsNeutralCulture, true);
         });
 
     test(
         "Checking the integrity of the invariant culture...",
         () =>
         {
-            assert.strictEqual(CultureInfo.InvariantCulture.Name, "");
-            assert.strictEqual(CultureInfo.InvariantCulture.toString(), "");
-            assert.strictEqual(new CultureInfo("de").Parent, CultureInfo.InvariantCulture);
-            assert.strictEqual(new CultureInfo("zh-Hans-CN").Parent.Parent.Parent, CultureInfo.InvariantCulture);
+            Assert.strictEqual(CultureInfo.InvariantCulture.Name, "");
+            Assert.strictEqual(CultureInfo.InvariantCulture.toString(), "");
+            Assert.strictEqual(new CultureInfo("de").Parent, CultureInfo.InvariantCulture);
+            Assert.strictEqual(new CultureInfo("zh-Hans-CN").Parent.Parent.Parent, CultureInfo.InvariantCulture);
         });
 });
